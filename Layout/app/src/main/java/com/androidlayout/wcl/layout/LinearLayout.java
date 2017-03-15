3ac37af060927854159a0e5a69db0e7bf493551f
default_server <a1234_df@163.com>
2017-03-15 19:53:59 +0800

    Signed-off-by: default_server <a1234_df@163.com>

---
+++ Layout/app/src/main/java/com/androidlayout/wcl/layout/LinearLayout.java
@@ -0,0 +1,17 @@
+package com.androidlayout.wcl.layout;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinearLayout extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
